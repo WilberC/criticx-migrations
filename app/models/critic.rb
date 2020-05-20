9cfb7430e55be7fd39e0a0b93f5303b72c29093b
unknown <wilber.carrascal@gmail.com>
2020-05-19 21:36:13 -0500

    validations

--- app/models/critic.rb
+++ app/models/critic.rb
@@ -1,3 +1,6 @@
 class Critic < ApplicationRecord
   belongs_to :game
+  validates :username, length: {minimum: 2, maximum: 12}
+  validates :user_name, with: /^[A-Za-z0-9]+$/
+  validates :title, :body, presence: {message: 'Please write something!'}
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,3 @@
 class Game < ApplicationRecord
+  validates :name, :genre, :price, :release_date, :company_id, presence: true
 end
